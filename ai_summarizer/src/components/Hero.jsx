4430b776135516c272800ff9e39207a08070ef2d
krunal16-c <krunalchavdaofficial13@gmail.com>
2023-05-27 02:29:12 -0600

    first-commit

---
+++ ai_summarizer/src/components/Hero.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { favicon } from '../assets'
+
+const Hero = () => {
+  return (
+    <header className="w-full flex justify-center items-center flex-col">
+        <nav className="flex justify-between items-center w-full mb-10 pt-3">
+            <img src={favicon} alt="sumz_logo" className="w-28 object-contain" />
+            <button
+            type="Button"
+            onClick={() => window.open('https://github.com/krunal16-c')}
+            className="black_btn"
+            >
+                GitHub
+            </button>
+        </nav>
+        <h1 className='head_text'>
+            Summarize Articles with <br className='max-md:hidden'/>
+            <span
+            className='orange_gradient'>OpenAI GPT-4 
+            </span>
+        </h1>
+        <h2 className='desc'>
+            Simplify your reading with Summize, an open-source article summarizer 
+            that transforms lengthy articles into clear and concise summaries.
+        </h2>
+    </header>
+  )
+}
+
+export default Hero
